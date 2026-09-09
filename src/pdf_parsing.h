dcd33531f1b04e6fc7cab51e73b1d78b471d3393
NasriAnis <nasrianis213@gmail.com>
2026-09-09 14:39:20 +0100

    added comments on helper functions

--- src/pdf_parsing.h
+++ src/pdf_parsing.h
@@ -7,6 +7,13 @@ typedef struct s_page_text {
     char *text;
 } s_page_text;
 
+/*
+ * this function takes a path to a pdf file
+ * and returns an heap allocated array of
+ * s_page_text structures
+ * which can be accessed one by one.
+ */
+
 s_page_text *get_page_text_from_pdf(char *path_pdf);
 
 #endif
--- src/txt_parsing.h
+++ src/txt_parsing.h
@@ -14,6 +14,14 @@ typedef struct {
     size_t capacity;
 } term_table;
 
+/*
+ * this fhunction takes a pointer to
+ * some space separated text and return
+ * a term_table struct per text where is
+ * each word and its count inside a term_count
+ * Struct.
+ */
+
 term_table count_words(char *text);
 void free_term_table(term_table *t);
 
