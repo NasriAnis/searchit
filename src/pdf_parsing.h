d6d3b7cd5e7a97380565356ba1fb405c8a90f1e4
NasriAnis <nasrianis213@gmail.com>
2026-09-10 21:37:13 +0100

    dummy comit

--- src/pdf_parsing.h
+++ src/pdf_parsing.h
@@ -12,7 +12,7 @@ typedef struct s_page_text {
  * and returns an heap allocated array of
  * s_page_text structures
  * which can be accessed one by one.
- */
+*/
 
 s_page_text *get_page_text_from_pdf(char *path_pdf);
 
