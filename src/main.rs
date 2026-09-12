3da869317b688ba8142406fcf073af23bb7fa444
NasriAnis <nasrianis213@gmail.com>
2026-09-12 18:22:19 +0100

    impl minimal cli help section and scan command call

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::env;
+
+pub mod cli;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        help()
+    }
+    else {
+        match args[1].as_str() {
+            "help" => { help() }
+            "scan" => { cli::scan::run(args) },
+            _ => {
+                eprintln!("ERROR: this command doesnt exit {}", args[1]);
+                eprintln!("EXITING!")
+            }
+        }
+    }
+}
+
+fn help(){
+    println!("Help section :
+    - help <command> : get help for a specific command
+    - scan <path> : scan a directory to prepare for searching
+    ")
 }
